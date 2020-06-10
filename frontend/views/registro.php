5228ff68baa111db7634705ba30a11b024b0d97d
Edangc <lalo.gutierrez.c@gmail.com>
2020-06-10 08:47:21 -0400

    jquery validate registro

--- frontend/views/registro.php
+++ frontend/views/registro.php
@@ -9,34 +9,48 @@
                 <div id="form-row" class="row justify-content-center align-items-center">
                     <div id="form-column" class="col-md-6">
                         <div id="form-box" class="bg-light text-dark">
-                            <form method="POST" action="../backend/controllers/registerCtr.php" enctype="multipart/form-data">
+                            <form id="registration" method="POST" enctype="multipart/form-data">
                                 <h3 class="text-center text-dark mb-3"> Registrarse</h3>
                                 <div class="form-group">
                                     <label for="fullname">Nombre completo (visible para todos)</label>
-                                    <input type="name" class="form-control"
+                                    <input id="nombre" type="name" class="form-control"
                                         placeholder="Ingresa tu nombre completo o un nombre de usuario" name="fullname">
                                 </div>
+                                <div class="error alert alert-danger" role="alert" id="mensaje1">Llenar campo nombre</div>
+
                                 <div class="form-group">
                                     <label for="email">Correo electrónico</label>
-                                    <input type="email" class="form-control" placeholder="Ingresa tu correo" name="email">
+                                    <input id="email" type="email" class="form-control" placeholder="Ingresa tu correo" name="email">
                                 </div>
+                                <div class="error alert alert-danger" role="alert" id="mensaje2">LLenar campo correo correctamente</div>
+
+
                                 <div class="form-group">
                                     <label for="password">Contraseña</label>
-                                    <input type="password" class="form-control" placeholder="Password" name="password">
+                                    <input id="password" type="password" class="form-control" placeholder="Password" name="password">
                                 </div>
+                                <div class="error alert alert-danger" role="alert" id="mensaje3">Llenar campo contraseña</div>
+                                <div class="error alert alert-danger" role="alert" id="mensajePw">La contraseña debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.
+                                NO puede tener otros símbolos.</div>
+
                                 <div class="form-group">
                                     <label for="image">Foto de perfil</label>
                                     <input type="file" class="form-control-file" name="image" accept="image/*">
                                 </div>
+
+
                                 <div class="form-group">
                                     <label for="description">Descripción</label>
-                                    <textarea class="form-control" rows="3" placeholder="Escribe sobre ti..."
+                                    <textarea id="description" class="form-control" rows="3" placeholder="Escribe sobre ti..."
                                         name="description"></textarea>
                                 </div>
+                                <div class="error alert alert-danger" role="alert" id="mensaje4">Llenar campo descripcion con maximo 2048 caracteres</div>
+
+
                                 <!-- BOTON -->
                                 <div class="form-group text-center">
-                                    <input type="submit" class="btn btn-block btn-lg btn-formulario border-dark" value="Registrarse">
-                                    <span class="text-danger" id="registerResponse"></span>
+                                    <input id="registerResponse" type="submit" class="btn btn-block btn-lg btn-formulario border-dark" value="Registrarse">
+                                    <span class="text-danger" ></span>
                                 </div>
                             </form>
                         </div>
